9027e384ce6f95b47c4563d0ed19c1389b1a0c78
DvirAmram <dvir.amram@live.biu.ac.il>
2023-04-09 21:47:36 +0300

    9/4

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ class User(db.Document):
     name = db.StringField(required=True)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
-    items = db.ListField(db.ReferenceField('Item'))
+    # buildings = db.ListField(db.ReferenceField('Building'))
 
     def to_dict(self):
         return {'id': str(self.id), 'name': self.name, 'email': self.email, 'password': self.password}
 
 
-class Item(db.Document):
-    name = db.StringField(required=True)
-    user_id = db.ReferenceField(User)
+class Building(db.Document):
+    address = db.StringField(required=True, unique=True)
+    tenants = db.ListField(db.ReferenceField('User'))
 
     def to_dict(self):
-        return {'id': str(self.id), 'name': self.name, 'user_id': str(self.user_id.id)}
+        return {'id': str(self.id), 'address': self.address, 'tenants': self.tenants}
 
 
 @app.route('/users', methods=['GET'])
@@ -71,14 +71,21 @@ def login():
 def register():
     try:
         data = request.args
-        email = data.get('email')
-        name = data.get('name')
-        password = data.get('password')
         user = User(name=data['name'], email=data['email'], password=data['password'])
         user.save()
         return jsonify(user.to_dict()), 201
     except KeyError:
         return jsonify({'error': 'Missing required field(s)'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/users/get_buildings_by_user', methods=['GET'])
+def get_buildings_by_user():
+    try:
+        data = request.args
+        user = User.objects.get(email=data["email"])
+        return jsonify([t.address for t in Building.objects(tenants__in=[user])]), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
@@ -130,80 +137,109 @@ def delete_user(id):
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/items', methods=['GET'])
-def get_all_items():
+@app.route('/buildings', methods=['GET'])
+def get_all_Buildings():
     try:
-        items = Item.objects()
-        return jsonify([item.to_dict() for item in items]), 200
+        Buildings = Building.objects()
+        return jsonify([Building.to_dict() for Building in Buildings]), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/items/<string:id>', methods=['GET'])
-def get_item(id):
+@app.route('/buildings/<string:id>', methods=['GET'])
+def get_Building(id):
     try:
-        item = Item.objects.get(id=id)
-        return jsonify(item.to_dict()), 200
+        building = Building.objects.get(id=id)
+        return jsonify(building.to_dict()), 200
     except DoesNotExist:
-        return jsonify({'error': 'Item not found'}), 404
+        return jsonify({'error': 'Building not found'}), 404
     except ValidationError:
-        return jsonify({'error': 'Invalid item ID'}), 400
+        return jsonify({'error': 'Invalid Building ID'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/items', methods=['POST'])
-def create_item():
+@app.route('/buildings/new_building', methods=['POST'])
+def create_Building():
     try:
-        # item_data = request.form
-        item_data = request.get_json()
-        user_id = item_data.get('user_id')
-        user = User.objects.get(id=user_id)
-        item = Item(name=item_data['name'], user_id=user)
-        user.items.append(item)
-        item.save()
-        user.save()
-        return jsonify(item.to_dict()), 201
+        data = request.args
+        # address = data.get('address')
+        building = Building(address=data['address'])
+        building.save()
+        return jsonify(building.to_dict()), 201
     except KeyError:
         return jsonify({'error': 'Missing required field(s)'}), 400
-    except DoesNotExist:
-        return jsonify({'error': 'User not found'}), 404
-    except ValidationError:
-        return jsonify({'error': 'Invalid user ID or item name'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/items/<string:id>', methods=['PUT'])
-def update_item(id):
+
+@app.route('/buildings/add_tenant_to_building', methods=['PUT'])
+def add_tenant_to_building():
     try:
-        # item_data = request.form
-        item_data = request.get_json()
-        item = Item.objects.get(id=id)
-        item.name = item_data.get('name', item.name)
-        if 'user_id' in item_data:
-            user = User.objects.get(id=item_data['user_id'])
-            item.user_id = user
-        item.save()
-        return jsonify(item.to_dict()), 200
-    except DoesNotExist:
-        return jsonify({'error': 'Item not found'}), 404
-    except ValidationError:
-        return jsonify({'error': 'Invalid item ID or user ID'}), 400
+        data = request.args
+        email = data.get('email')
+        address = data.get('address')
+        user = User.objects.get(email=email)
+        building = Building.objects.get(address=address)
+        if user in building.tenants:
+            return jsonify({'error': 'The tenant is already registered in this building'}), 500
+        building.tenants.append(user)
+        building.save()
+        return jsonify(building.to_dict()), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 
-@app.route('/items/<string:id>', methods=['DELETE'])
-def delete_item(id):
+@app.route('/buildings/remove_tenant_to_building', methods=['PUT'])
+def remove_tenant_to_building():
     try:
-        item = Item.objects.get(id=id)
-        item.delete()
+        data = request.args
+        email = data.get('email')
+        address = data.get('address')
+        user = User.objects.get(email=email)
+        building = Building.objects.get(address=address)
+        Building.objects(id=building.id).update_one(pull__tenants=user)
+        # if user in building.tenants:
+        #     return jsonify({'error': 'The tenant is already registered in this building'}), 500
+        # building.tenants.append(user)
+        # building.save()
+        return jsonify(building.to_dict()), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+
+@app.route('/Buildings/<string:id>', methods=['PUT'])
+def update_Building(id):
+    try:
+        # Building_data = request.form
+        Building_data = request.get_json()
+        building = Building.objects.get(id=id)
+        Building.name = Building_data.get('name', Building.name)
+        if 'user_id' in Building_data:
+            user = User.objects.get(id=Building_data['user_id'])
+            Building.user_id = user
+        Building.save()
+        return jsonify(building.to_dict()), 200
+    except DoesNotExist:
+        return jsonify({'error': 'Building not found'}), 404
+    except ValidationError:
+        return jsonify({'error': 'Invalid Building ID or user ID'}), 400
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/Buildings/<string:id>', methods=['DELETE'])
+def delete_Building(id):
+    try:
+        building = Building.objects.get(id=id)
+        Building.delete()
         return '', 204
     except DoesNotExist:
-        return jsonify({'error': 'Item not found'}), 404
+        return jsonify({'error': 'Building not found'}), 404
     except ValidationError:
-        return jsonify({'error': 'Invalid item ID'}), 400
+        return jsonify({'error': 'Invalid Building ID'}), 400
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
